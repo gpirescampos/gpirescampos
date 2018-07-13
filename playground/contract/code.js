68f5f50bab74ce2d4e498009c8695af2e1d241a0
Guilherme Campos <guilherme.campos@consenys.net>
2018-07-14 00:26:49 +0100

    decided to go with the neon page

--- playground/contract/code.js
+++
@@ -1,9 +0,0 @@
-contractLocation = 0x02c565ba6fffe9b9197e94e1d42901c51a80e21d
-
-contract = 
-  
-pragma solidity ^0.4.16;
-
-contract HelloWorld {
-    string public helloWorld = "Hello World!";
-}
